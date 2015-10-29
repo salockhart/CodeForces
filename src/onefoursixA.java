91a80d8c65abb682b5bbf2e1f814036e4ebe65d2
Alex <salexlockhart@gmail.com>
2015-10-29 15:42:38 -0300

    Finish #146A

---
+++ src/onefoursixA.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * Created by Alex on 2015-10-29.
+ */
+public class onefoursixA {
+
+    public static void main(String args[]) {
+
+        //Input
+        Scanner input = new Scanner(System.in);
+
+        //Get length
+        int n = input.nextInt();
+
+        //Get number
+        String num = input.next();
+
+        //Flag
+        boolean flag = true;
+
+        //Determine if lucky
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0; i < num.length() && flag; i++) {
+            if (num.charAt(i) != '4' && num.charAt(i) != '7') {
+                flag = false;
+            } else if (i < num.length()/2) {
+                leftSum += Character.getNumericValue(num.charAt(i));
+            } else {
+                rightSum += Character.getNumericValue(num.charAt(i));
+            }
+        }
+
+        if (leftSum != rightSum) flag = false;
+
+        if (flag) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+}
